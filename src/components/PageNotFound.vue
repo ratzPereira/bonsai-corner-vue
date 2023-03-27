6a662e1ac5cce3aae1e98a17494bb06c7ff6e6c8
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-03-27 10:19:44 +0000

    created generic page not found component

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps(['message'])
+</script>
+
+<template>
+  <div class="page-not-found">
+    <h1>Oops!</h1>
+    <p>{{ props.message ? props.message : "Sorry, the page you are looking for cannot be found." }}</p>
+  </div>
+</template>
+
+<style scoped>
+.page-not-found {
+  background-color: aliceblue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  font-size: 5rem;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 2rem;
+  text-align: center;
+  max-width: 50rem;
+  margin-bottom: 2rem;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProfileView from "@/views/ProfileView.vue";
 import { authGuard } from "@/guards/AuthGuard";
+import PageNotFound from "@/components/PageNotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       name: "profile",
       component: ProfileView,
       beforeEnter: authGuard,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "page not found",
+      component: PageNotFound,
     },
   ],
 });
