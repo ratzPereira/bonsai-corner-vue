08af6787259f7d5c5d92d6e29c8f64e489ddc54c
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-03-30 20:05:00 +0000

    refactored all the create new post. implemented the photo upload to supabase

--- src/components/CreatePostModal.vue
+++ src/components/CreatePostModal.vue
@@ -1,29 +1,104 @@
 <script setup>
 
-import {ref} from "vue";
+import {reactive, ref} from "vue";
+import {usePostsStore} from "@/stores/posts";
+import {storeToRefs} from "pinia";
+import {supabase} from "@/supabase";
+
+const postStore = usePostsStore()
+const {errorMessage, loading} = storeToRefs(postStore)
 
 const visible = ref(false);
+const imagesToUpload = reactive([])
+
+const post = reactive({
+  name: '',
+  age: '',
+  description: '',
+  images: [],
+  species: '',
+  interventions: '',
+})
+
+
 const showModal = () => {
+  errorMessage.value = ''
   visible.value = true;
 };
 const handleOk = e => {
-  console.log(e);
-  visible.value = false;
+
+  loading.value = true
+  console.log('imagens na array')
+  console.log(imagesToUpload)
+  uploadImagesToBucket(imagesToUpload)
+  postStore.handleNewPost(post)
+  //visible.value = false;
 };
+
+
+const handleUploadChange = (event) => {
+  const files = event.target.files;
+  for (let i = 0; i < files.length; i++) {
+    imagesToUpload.push(files[i]);
+  }
+};
+
+const uploadImagesToBucket = async (images) => {
+  console.log('imagens a chegar')
+  console.log({images})
+  await images.forEach((image) => {
+    const fileName = Math.floor(Math.random() * 1000000000000)
+    const response = supabase.storage.from('bonsai').upload('public/' + fileName, image)
+    console.log({response})
+  })
+}
 
 </script>
 
 <template>
   <div>
-    <AButton class="btn" type="primary" @click="showModal">coiso</AButton>
-    <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk">
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-    </a-modal>
+    <AButton class="btn" type="primary" @click="showModal">New Post</AButton>
+    <AModal v-model:visible="visible" title="New Post" @ok="handleOk">
+      <div v-if="!loading">
+        <AForm :form="post">
+          <AFormItem label="Name" required>
+            <AInput v-model:value="post.name"/>
+          </AFormItem>
+          <AFormItem label="Age" required>
+            <AInputNumber v-model:value="post.age"/>
+          </AFormItem>
+          <AFormItem label="Description" required>
+            <ATextarea v-model:value="post.description"/>
+          </AFormItem>
+          <AFormItem label="Images" required>
+            <input
+                :multiple="true"
+                accept=".png,.jpeg"
+                type="file"
+                @change="handleUploadChange"
+            >
+          </AFormItem>
+          <AFormItem label="Species" required>
+            <AInput v-model:value="post.species"/>
+          </AFormItem>
+          <AFormItem label="Interventions">
+            <AInput v-model:value="post.interventions"/>
+          </AFormItem>
+        </AForm>
+      </div>
+      <div v-else class="spinner">
+        <ASpin></ASpin>
+      </div>
+      <ATypographyText v-if="errorMessage" type="danger">{{ errorMessage }}</ATypographyText>
+    </AModal>
   </div>
 </template>
 
 <style scoped>
-
+.spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 120px;
+}
 </style>
--- src/stores/posts.js
+++ src/stores/posts.js
@@ -7,6 +7,8 @@ export const usePostsStore = defineStore("posts", () => {
   const user = useUserStore();
 
   const posts = ref([]);
+  const loading = ref(false);
+  const errorMessage = ref("");
 
   const getPosts = async () => {
     await axios
@@ -17,8 +19,51 @@ export const usePostsStore = defineStore("posts", () => {
       })
       .catch((error) => {});
   };
+
+  const handleNewPost = async (post) => {
+    loading.value = true;
+
+    const { name, age, description, images, species, interventions } = post;
+
+    validatePost(name, errorMessage, description, species, age, images);
+
+    loading.value = false;
+  };
+
+  const validatePost = (
+    name,
+    errorMessage,
+    description,
+    species,
+    age,
+    images
+  ) => {
+    if (String(name) <= 3) {
+      return (errorMessage.value = "Please insert valid name!");
+    }
+
+    if (String(description) <= 5) {
+      return (errorMessage.value = "The description is too short");
+    }
+
+    if (String(species) <= 3) {
+      return (errorMessage.value = "The species name is too short");
+    }
+
+    if (age < 0) {
+      return (errorMessage.value = "The age must be greater than zero");
+    }
+
+    if (!images) {
+      return (errorMessage.value = "Please add at least one image");
+    }
+  };
+
   return {
     posts,
+    errorMessage,
+    loading,
     getPosts,
+    handleNewPost,
   };
 });
